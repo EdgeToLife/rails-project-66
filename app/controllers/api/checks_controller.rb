092c3f4c4a387904a513fb5f9efc84948de2b6df
Ivan Zhukov <ziv@servers.com>
2024-03-17 14:00:13 +0200

    fix: added check in API check that repository exists

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -9,6 +9,12 @@ module Api
         payload = JSON.parse(request.body.read)
         repo_id = payload['repository']['id']
         repository = Repository.find_by(github_id: repo_id)
+
+        if repository.nil?
+          render json: { error: 'Repository not found' }, status: :not_found
+          return
+        end
+
         check = repository.checks.build
         repository_analyzer_job = ApplicationContainer[:repository_analyzer_job]
         repository_analyzer_job.perform_later(check, repository.user.id)
