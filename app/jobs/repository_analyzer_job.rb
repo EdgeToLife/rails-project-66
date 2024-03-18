eadca28fed3861ce5df24aa2486b52d54e33aded
Ivan Zhukov <ziv@servers.com>
2024-03-18 02:55:58 +0200

    fix: optimized parse_and_update_check_data method

--- app/jobs/repository_analyzer_job.rb
+++ app/jobs/repository_analyzer_job.rb
@@ -29,23 +29,14 @@ class RepositoryAnalyzerJob < ApplicationJob
   private
 
   def analyze_repository(client, check, download_path)
-    if check.repository.language == 'javascript'
-      eslintrc_path = Rails.root.join('.eslintrc.yml')
-      cmd = "npx eslint #{download_path} --no-eslintrc --config #{eslintrc_path} -f json"
-      stdout, exit_status = Open3.popen3(cmd) do |_stdin, inner_stdout, _stderr, wait_thr|
-        [inner_stdout.read, wait_thr.value]
-      end
-    elsif check.repository.language == 'ruby'
-      rubocop_conf_path = Rails.root.join('.rubocop_ext.yml')
-      cmd = "rubocop #{download_path} -c #{rubocop_conf_path} --format json"
-      stdout, exit_status = Open3.popen3(cmd) do |_stdin, inner_stdout, _stderr, wait_thr|
-        [inner_stdout.read, wait_thr.value]
-      end
+    cmd = make_linter_cmd(check, download_path)
+    stdout, exit_status = Open3.popen3(cmd) do |_stdin, inner_stdout, _stderr, wait_thr|
+      [inner_stdout.read, wait_thr.value]
     end
-    data = JSON.parse(stdout)
     passed = exit_status.success?
     commit_id = client.commits(check.repository[:full_name]).first.sha[0, 7]
-    parse_and_update_check_data(check, data, commit_id, passed, download_path)
+    data, error_count = parse_check_data(check, stdout, download_path)
+    check.update!(data:, error_count:, commit_id:, passed:)
   end
 
   def download_repository(client, repo_full_name, path, download_path)
@@ -65,15 +56,25 @@ class RepositoryAnalyzerJob < ApplicationJob
     end
   end
 
+  def make_linter_cmd(check, download_path)
+    if check.repository.language == 'javascript'
+      eslintrc_path = Rails.root.join('.eslintrc.yml')
+      cmd = "npx eslint #{download_path} --no-eslintrc --config #{eslintrc_path} -f json"
+    elsif check.repository.language == 'ruby'
+      rubocop_conf_path = Rails.root.join('.rubocop_ext.yml')
+      cmd = "rubocop #{download_path} -c #{rubocop_conf_path} --format json"
+    end
+  end
+
   def notify_user(check, user)
     CheckMailer.with(user:, repository: check.repository).check_fail_notification.deliver_now
   end
 
-  def parse_and_update_check_data(check, data, commit_id, passed, download_path)
+  def parse_check_data(check, stdout, download_path)
+    data = JSON.parse(stdout)
     formatter_class_name = "#{check.repository.language.capitalize}Formatter"
     formatter_class = formatter_class_name.safe_constantize
     formatted_data, total_error_count = formatter_class.format_data(data, download_path)
-    check.update!(data: formatted_data, error_count: total_error_count, commit_id:, passed:)
   end
 
   def prepare_download_path(repo_full_name)
