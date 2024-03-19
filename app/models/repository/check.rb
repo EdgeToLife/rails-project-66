86032e8efed0ea970a444ba45f13a4f2f3f04dd7
Ivan Zhukov <ziv@servers.com>
2024-03-20 00:51:59 +0200

    fix: corrected 'check' model structure

--- app/models/repository/check.rb
+++ app/models/repository/check.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
-module Repositories
-  class Repository::Check < ApplicationRecord
-    include AASM
+class Repository::Check < ApplicationRecord
+  include AASM
 
-    aasm column: 'aasm_state' do
-      state :not_started, initial: true
-      state :started, :finished, :failed
+  aasm column: 'aasm_state' do
+    state :not_started, initial: true
+    state :started, :finished, :failed
 
-      event :start do
-        transitions from: %i[not_started], to: :started
-      end
-
-      event :finish do
-        transitions from: :started, to: :finished
-      end
-
-      event :fail do
-        transitions from: :started, to: :failed
-      end
+    event :start do
+      transitions from: %i[not_started], to: :started
     end
 
-    belongs_to :repository
+    event :finish do
+      transitions from: :started, to: :finished
+    end
+
+    event :fail do
+      transitions from: :started, to: :failed
+    end
   end
+
+  belongs_to :repository
 end
