0aa8e2e09a621a9492a4f845f130f9326ab81395
Ivan Zhukov <ziv@servers.com>
2024-03-20 01:56:02 +0200

    fix: improved 'check' tests

--- test/controllers/checks_controller_test.rb
+++ test/controllers/checks_controller_test.rb
@@ -5,10 +5,18 @@ require 'test_helper'
 class ChecksControllerTest < ActionDispatch::IntegrationTest
   test 'should create check repository via API' do
     repository = repositories(:repo_one)
+    attrs = {
+      aasm_state: 'finished',
+      passed: true,
+      error_count: 0,
+      commit_id: 'fcad3c9',
+    }
     json_payload = { repository: { id: repository.github_id } }
     assert_difference('Repository::Check.count') do
       post api_checks_path, params: json_payload, as: :json
     end
     assert_response :success
+    check = Repository::Check.find_by(attrs)
+    assert { check }
   end
 end
--- test/controllers/repositories/check_controller_test.rb
+++ test/controllers/repositories/check_controller_test.rb
@@ -5,21 +5,29 @@ require 'test_helper'
 class ChecksControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    @repository = repositories(:repo_one)
-    @check = repository_checks(:one)
   end
 
   test 'should show check' do
+    check = repository_checks(:one)
     sign_in @user
-    get repository_check_url(repository_id: @check.repository_id, id: @check.id)
+    get repository_check_url(repository_id: check.repository_id, id: check.id)
     assert_response :success
   end
 
   test 'should create check repository' do
+    repo_without_checks = repositories(:repo_two)
+    attrs = {
+      aasm_state: 'finished',
+      passed: true,
+      error_count: 0,
+      commit_id: 'fcad3c9',
+    }
     sign_in @user
     assert_difference('Repository::Check.count') do
-      post repository_checks_path(@repository)
+      post repository_checks_path(repo_without_checks)
     end
-    assert_redirected_to repository_path(@repository)
+    assert_redirected_to repository_path(repo_without_checks)
+    check = Repository::Check.find_by(attrs)
+    assert { check }
   end
 end
